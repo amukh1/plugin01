e35c2744b171dc671bf151d8e8aa7c65bfd985fb
Amukh1 <85003861+amukh1@users.noreply.github.com>
2021-09-04 18:08:57 -0400

    Update index.plugin.js

--- index.plugin.js
+++ index.plugin.js
@@ -8,7 +8,7 @@ class ExampleLibraryPlugin {
 
     start() {
         if (!global.ZeresPluginLibrary) return window.BdApi.alert("Library Missing",`The library plugin needed for ${this.getName()} is missing.<br /><br /> <a href="https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js" target="_blank">Click here to download the library!</a>`);
-        ZLibrary.PluginUpdater.checkForUpdate(this.getName(), this.getVersion(), "");
+        ZLibrary.PluginUpdater.checkForUpdate(this.getName(), this.getVersion(), "https://raw.githubusercontent.com/amukh1/plugin01/main/index.plugin.js");
     }
 
     stop() {

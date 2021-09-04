97bcde8cfdad03767704a49ae094ea334238e4da
Amukh1 <85003861+amukh1@users.noreply.github.com>
2021-09-03 23:51:02 -0400

    Create testtt01.plugin.js

---
+++ testtt01.plugin.js
@@ -0,0 +1,23 @@
+/**
+ * @name Test
+ * @version 0.0.1
+ * @description Description
+ * @author Amukh1
+ *  
+ */
+
+
+
+ module.exports = class Example{
+     
+    load() { }
+    start() {
+
+//code here
+BdApi.alert('hello world')
+//stop coding
+}
+stop(){
+
+}
+}
